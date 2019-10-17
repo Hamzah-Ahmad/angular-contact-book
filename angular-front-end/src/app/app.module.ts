70f4986f347984e2d76cc14fa0460140052545d0
unknown <ahmadhamzah500@gmail.com>
2019-10-17 16:33:23 -0500

    Implemented firebase authentication alongwith route guards

--- angular-front-end/src/app/app.module.ts
+++ angular-front-end/src/app/app.module.ts
@@ -2,16 +2,56 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { Routes, RouterModule, CanActivate } from "@angular/router";
+
+import { AngularFireModule } from "angularfire2";
+import { AngularFireAuthModule } from "angularfire2/auth";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ContactComponent } from "./contact/contact.component";
 import { NewContactComponent } from "./new-contact/new-contact.component";
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
+import { AuthService } from "./auth.service";
+import { AuthGuardService } from "./auth-guard.service";
+
+export var firebaseConfig = {
+  apiKey: "AIzaSyAb5Z3I3RCoRv4ymqd0UYCzGZjvqcY5blc",
+  authDomain: "angular-contacts-a64f3.firebaseapp.com",
+  databaseURL: "https://angular-contacts-a64f3.firebaseio.com",
+  projectId: "angular-contacts-a64f3",
+  storageBucket: "angular-contacts-a64f3.appspot.com",
+  messagingSenderId: "889277044775",
+  appId: "1:889277044775:web:dbba0ba7deb9834dcd9602"
+};
 
 @NgModule({
-  declarations: [AppComponent, ContactComponent, NewContactComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    ContactComponent,
+    NewContactComponent,
+    LoginComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: "",
+        component: ContactComponent,
+        canActivate: [AuthGuardService]
+      },
+      { path: "login", component: LoginComponent },
+      { path: "signup", component: SignupComponent }
+    ]),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
+  ],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ angular-front-end/src/app/auth.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+
+import { AngularFireAuth } from "angularfire2/auth";
+import * as firebase from "firebase/app";
+
+import { Observable } from "rxjs";
+@Injectable()
+export class AuthService {
+  user: Observable<firebase.User>;
+
+  constructor(private firebaseAuth: AngularFireAuth, private router: Router) {
+    this.user = firebaseAuth.authState;
+  }
+
+  signup(email: string, password: string) {
+    this.firebaseAuth.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(value => {
+        console.log("Success!", value);
+        this.router.navigate(["/"]);
+      })
+      .catch(err => {
+        console.log("Something went wrong:", err.message);
+      });
+  }
+
+  login(email: string, password: string) {
+    this.firebaseAuth.auth
+      .signInWithEmailAndPassword(email, password)
+      .then(value => {
+        console.log("Nice, it worked!");
+        this.router.navigate(["/"]);
+      })
+      .catch(err => {
+        console.log("Something went wrong:", err.message);
+      });
+  }
+
+  logout() {
+    this.firebaseAuth.auth.signOut().then(() => {
+      this.router.navigate(["login"]);
+    });
+  }
+
+  isLoggedIn() {
+    var user = firebase.auth().currentUser;
+
+    if (user) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
